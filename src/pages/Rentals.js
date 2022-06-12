d196a83836d8d11d057609e8c6e13a56f76d88c5
darshil7056 <darshilpatel7056@gmail.com>
2022-06-13 03:05:24 +0530

    Created Home Page

--- src/pages/Rentals.js
+++ src/pages/Rentals.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Rentals.css";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Rentals = () => {
+  const {state:searchFilters} = useLocation()
 
   return (
     <>
      <Link to="/"> Home </Link>
+    { searchFilters && 
+    <div onClick = {() =>console.log(searchFilters)}>Helo</div>
+    }
     </>
   );
 };
